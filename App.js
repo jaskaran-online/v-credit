0dc1d580318a9070b39e62c6f3452be79ee49d64
Jaskaran Singh <42887065+dev-jaskaranSingh@users.noreply.github.com>
2023-04-30 20:09:40 +0530

    Install React Query : Basic setup done Install Axios package

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <QueryClientProvider client={queryClient}>
+        <View className="flex-1 items-center justify-center bg-white">
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </QueryClientProvider>
   );
 }
