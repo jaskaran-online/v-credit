117e1615be659a520c0900c498e0efb442d2c07c
Jaskaran Singh <jaskaransingh4704@gmail.com>
2023-11-26 18:49:20 +0530

    Upgrade app to latest sdk 49 and made changed in android ui

--- src/screens/Components/TabNavigator.js
+++ src/screens/Components/TabNavigator.js
@@ -9,18 +9,19 @@ const Tab = createMaterialTopTabNavigator();
 export function TabNavigator() {
   const screenOptions = {
     tabBarLabelStyle: {
-      fontSize: 13,
-      fontWeight: '600',
+      fontSize: 12,
+      fontWeight: '400',
     },
     tabBarStyle: {
       backgroundColor: 'white',
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-      shadowOpacity: 0.18,
+      // shadowOffset: {
+      //   width: 0,
+      //   height: 1,
+      // },
+      // shadowOpacity: 0.18,
       shadowRadius: 5.0,
-      elevation: 1,
+      elevation: 5,
+      shadowColor: 'black',
     },
     tabBarActiveTintColor: COLORS.primary,
     tabBarInactiveTintColor: COLORS.darkgray,
