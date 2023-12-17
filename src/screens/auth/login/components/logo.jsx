fa05389abcc63f574f9ac5b9f583e07cba704a22
Jaskaran Singh <jaskaransingh4704@gmail.com>
2023-12-17 21:23:02 +0530

    Fixed unresolved image path issue

--- src/screens/auth/login/components/logo.jsx
+++ src/screens/auth/login/components/logo.jsx
@@ -2,6 +2,6 @@ import { Image } from 'react-native';
 
 export function Logo() {
   return (
-    <Image source={require('../../../../assets/logo.png')} className="m-auto h-[150] w-[150]" />
+    <Image source={require('../../../../../assets/logo.png')} className="m-auto h-[150] w-[150]" />
   );
 }
