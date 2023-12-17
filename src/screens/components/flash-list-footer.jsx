ead3a5d441d95e97922fe4014d6a9792f1c6948b
Jaskaran Singh <jaskaransingh4704@gmail.com>
2023-12-17 21:25:12 +0530

    Fixed unresolved style path issue

--- src/screens/components/flash-list-footer.jsx
+++ src/screens/components/flash-list-footer.jsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 
-import { styles } from './styles';
+import { styles } from '../styles';
 
 const FlashListFooter = () => <View style={styles.listFooter} />;
 export default FlashListFooter;
